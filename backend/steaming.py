60628bea45bf8154a3e3d48d765f5df14cd1ea30
Jiachen Li <alexjiachenli@hotmail.com>
2022-04-23 23:49:06 +1000

    Create steaming.py

---
+++ backend/steaming.py
@@ -0,0 +1,50 @@
+from tweepy import StreamingClient, Tweet, StreamRule
+
+import secret
+
+bearer_token = secret.BEARER_TOKEN
+
+
+class TweetListener(StreamingClient):
+
+    def on_tweet(self, tweet: Tweet):
+        print(tweet.__repr__())
+        
+    def on_request_error(self, status_code):
+        print(status_code)
+    
+    def on_connection_error(self):
+        self.disconnect
+        
+client = TweetListener(bearer_token)
+
+rules = [
+    StreamRule(value="Melbourne lang:en")
+    # StreamRule(value=""),
+    # StreamRule(value="bounding_box:[144.3896 -38.5084 145.5459 -37.3127]")
+    
+]
+
+
+resp = client.get_rules()
+if resp and resp.data:
+    rule_ids = []
+    for rule in resp.data:
+        rule_ids.append(rule.id)
+    client.delete_rules(rule_ids)
+    
+    
+resp = client.add_rules(rules, dry_run= True)
+if resp.errors:
+    raise RuntimeError(resp.errors)
+
+resp = client.add_rules(rules)
+if resp.errors:
+    raise RuntimeError(resp.errors)
+
+print(client.get_rules())
+
+try:
+    client.filter()
+except KeyboardInterrupt:
+    client.disconnect()
