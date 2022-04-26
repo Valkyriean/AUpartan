9c62265014e8a1b7418d96985d87155f2df80cfc
Jiachen Li <alexjiachenli@hotmail.com>
2022-04-26 16:21:33 +1000

    cors for front end testing

--- frontend/api/api.py
+++ frontend/api/api.py
@@ -1,7 +1,10 @@
 from os import system
 from flask import Flask, request
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/map', methods = ['POST'])
 def get_map_string():
