5d6a7fb4a06da5c94fe43fc89daf9353630290f4
Lupercal0 <55178796+Lupercal0@users.noreply.github.com>
2022-04-27 02:25:55 +1000

    SET new secret to global environment

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import subprocess
-import secret
+import mysecrets
 import importlib.util
 
 '''
@@ -24,12 +24,15 @@ tweepy = importlib.util.find_spec("tweepy")
 if flask is None or gunicorn is None or tweepy is None:
     subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
 
-os.environ['SECRET_KEY'] = secret.SECRET_KEY
-os.environ['BEARER_TOKEN'] = secret.BEARER_TOKEN
-os.environ['DB_USERNAME'] = secret.DB_USERNAME
-os.environ['DB_PASSWORD'] = secret.DB_PASSWORD
+os.environ['SECRET_KEY'] = mysecrets.SECRET_KEY
+os.environ['BEARER_TOKEN'] = mysecrets.BEARER_TOKEN
+os.environ['DB_USERNAME'] = mysecrets.DB_USERNAME
+os.environ['DB_PASSWORD'] = mysecrets.DB_PASSWORD
+os.environ['KEY']=mysecrets.KEY
+os.environ['KEYSECRET']=mysecrets.KEYSECRET
+
 if dev:
     os.environ['FLASK_ENV'] = "development"
-    os.system('cd backend && python3 -m flask run --host=0.0.0.0')
+    os.system('cd backend && python -m flask run --host=0.0.0.0')
 else:
-    os.system('cd backend && python3 -m gunicorn app:app')
+    os.system('cd backend && python -m gunicorn app:app')
