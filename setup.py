0cc1e8bfe68f8c2655bc19814f753b3147b3f329
Jiachen Li <alexjiachenli@hotmail.com>
2022-04-27 02:41:46 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import os
 import sys
 import subprocess
-import mysecrets
+import secret
 import importlib.util
+import platform
 
 '''
 Running instruction:
@@ -24,15 +25,21 @@ tweepy = importlib.util.find_spec("tweepy")
 if flask is None or gunicorn is None or tweepy is None:
     subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
 
-os.environ['SECRET_KEY'] = mysecrets.SECRET_KEY
-os.environ['BEARER_TOKEN'] = mysecrets.BEARER_TOKEN
-os.environ['DB_USERNAME'] = mysecrets.DB_USERNAME
-os.environ['DB_PASSWORD'] = mysecrets.DB_PASSWORD
-os.environ['KEY']=mysecrets.KEY
-os.environ['KEYSECRET']=mysecrets.KEYSECRET
+os.environ['SECRET_KEY'] = secret.SECRET_KEY
+os.environ['BEARER_TOKEN'] = secret.BEARER_TOKEN
+os.environ['DB_USERNAME'] = secret.DB_USERNAME
+os.environ['DB_PASSWORD'] = secret.DB_PASSWORD
+os.environ['KEY']=secret.KEY
+os.environ['KEYSECRET']=secret.KEYSECRET
 
 if dev:
     os.environ['FLASK_ENV'] = "development"
-    os.system('cd backend && python -m flask run --host=0.0.0.0')
+    if platform.system() == "Windows":
+        os.system('cd backend && flask run --host=0.0.0.0 --port=5001')
+    else:
+        os.system('cd backend && python3 -m flask run --host=0.0.0.0 --port=5001')
 else:
-    os.system('cd backend && python -m gunicorn app:app')
+    if platform.system() == "Windows":
+        os.system('cd backend && gunicorn app:app')
+    else:
+        os.system('cd backend && python3 -m gunicorn app:app')
