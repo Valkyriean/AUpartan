62758e10abd3632515d9099b577ec8bcd74a5380
Chi Zhang <Relaxrelaxzhang1017@gmail.com>
2022-04-28 01:56:20 +1000

    sa3 geo-location data upload

--- backend/geosa3.py
+++ backend/geosa3.py
@@ -1,79 +1,63 @@
-import json
 import math
+import json
+import pandas as pd
 import geopandas as gpd 
 from shapely.geometry import Point
-from flask import Blueprint
-from app import db_enable, couch
-from flaskext.couchdb import Document, CouchDBManager
-from couchdb.mapping import TextField
-from couchdb.design import ViewDefinition
 
-bp = Blueprint("geosa3", __name__, url_prefix="/geosa3")
+# Extract valid SA3 Code from AURIN dataset
+melb_sa3 = []
+income_level = "median_aud_2017_18"
+file_income = "../Data/Aurin/income.json"
+sa3 = "sa3_code"
+income_list = {}
+with open(file_income, 'r') as f:
+    objects = json.load(f)
 
-# Setup database in couchdb for stroing tweet information
-if db_enable:
-    try:
-        db = couch['geodata']
-    except:
-        db = couch.create('geodata')
-
-manager = CouchDBManager()
-
-# Setup views and designed documents for storing and querying SA3 geo-information
-view = ViewDefinition("tweet", 'value', '''
-    function(doc){
-        if (doc.doc_type == 'tweet'){
-            emit(doc.id, doc.text);
-        };
-    }''')
-
-class sa3geo(Document):
-    doc_type = 'sa3geo'
-    _id = TextField()
-    sa3_name = TextField()
-    coor_info = TextField()
-
-manager.add_document(sa3geo)
+    for i in objects["features"]:
+        instance = i["properties"]
+        melb_sa3.append(str(instance["sa3_code"]))
 
 # Collect SA3 code of all regions downloads from Aurin
 sf = gpd.read_file("../Data/SA3_2021_AUST_SHP_GDA2020/SA3_2021_AUST_GDA2020.shp")
-SA3_group = []
-for i in (sf["SA3_CODE21"]):
-    SA3_group.append(i)
+sf_len = len(sf["SA3_CODE21"])
 
-# Extract SA3 information in Greater Melbourne and Store them into couchdb
-@bp.route("/")
-def store_sa3geo():
-    filename = "../Data/Aurin/payroll.json"
-    with open(filename, 'r') as f:
-        objects = json.load(f)
+SA3_codes = []
+SA3_names = []
+SA3_coords = []
+SA3_radius = []
+SA3_coorstr = []
 
-        for i in objects["features"]:
+for i in range(sf_len):
+    sa3_info = (sf.iloc[[i]])
+    sa3_code = (sa3_info["SA3_CODE21"].tolist())[0]
 
-            # Extract instance from json
-            instance = i["properties"]
+    if sa3_code in melb_sa3:
+        SA3_codes.append(sa3_code)
+    else:
+        continue
 
-            # Find out corresponding SA3 regions
-            sa3_code = str(instance["sa3_code16"])
-            sa3_data_row = sf.iloc[[SA3_group.index(sa3_code)]]
+    # Estimates radius of SA3 polygons for further usage for twitter search region
+    sa3_a = math.sqrt(sa3_info["geometry"].area) * 100
+    sa3_radius = round(sa3_a / 2, 5)
 
-            # Estimates radius of SA3 polygons for further usage for twitter search region
-            sa3_radius = round(math.sqrt(sa3_data_row["geometry"].area) * 100 / 2, 5)
+    # Record the SA3 names
+    sa3_name = (sa3_info["SA3_NAME21"].tolist())[0]
+    SA3_names.append(sa3_name)
 
-            # Record the SA3 names
-            sa3_name = (sa3_data_row["SA3_NAME21"].tolist())[0]
-
-            # Calculate centroid of each polygon
-            sa3_centroid = sa3_data_row["geometry"].centroid
-            list_coor = Point(sa3_centroid.y, sa3_centroid.x)
-            
-            # Deliver coordinates and radius inforation for further usage of twitter harvesting
-            coor_info = str(round(list_coor.x, 5)) + "," + str(round(list_coor.y, 5)) + "," + str(sa3_radius) + "km"
-
-            # Store the SA3 information into couchdb without duplication
-            if (db_enable):
-                if (sa3_code) not in db:
-                    new_sa3geo = sa3geo(_id = sa3_code, sa3_name = sa3_name, coor_info = coor_info)
-                    new_sa3geo.store(db)
+    # Calculate bounding box of each polygon
+    sa3_centroid = sa3_info["geometry"].centroid
+    list_coor = Point(sa3_centroid.y, sa3_centroid.x)
+    lat_up = round(list_coor.x + sa3_a / 222, 6)
+    lat_down = round(list_coor.x - sa3_a / 222, 6)
+    lon_left = round(list_coor.y - sa3_a / math.cos(39), 6)
+    log_right = round(list_coor.y + sa3_a / math.cos(39), 6)
+    four_corner = [lon_left, lat_down, log_right, lat_up]
+    SA3_coords.append(four_corner)
     
-    return ("done")
+    # Deliver coordinates and radius inforation for further usage of twitter harvesting
+    coor_info = str(round(list_coor.x, 5)) + "," + str(round(list_coor.y, 5)) + "," + str(sa3_radius) + "km"
+    SA3_coorstr.append(coor_info)
+
+# Save SA3 geo-information as csv for further usage
+df = pd.DataFrame(list(zip(SA3_codes, SA3_names, SA3_coords, SA3_coorstr)), columns = ["SA3_CODE", "SA3_NAME", "SA3_COORDS", "SA3_GEOINFO"])
+df.to_csv("../Data/Aurin/sa3_geoinfo.csv", index = False)
