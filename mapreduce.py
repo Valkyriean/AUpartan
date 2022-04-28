7b7d6662115f4b516df8ba787791a6b1f2df71d5
Lupercal0 <55178796+Lupercal0@users.noreply.github.com>
2022-04-28 23:14:09 +1000

    add file including possible functional map-reduce function
    use to calculate average function

---
+++ mapreduce.py
@@ -0,0 +1,20 @@
+from flaskext.couchdb import Document, CouchDBManager,ViewDefnition
+
+#this should return total value of nlpvalue attribute
+average = viewDefnition('doc name here','average','''/
+    function(doc){
+        emit(doc.sa3_id, doc.nlpvalue);
+    }''','''function(keys, values, rereduce){
+            return sum(values);
+    }
+    '''
+)
+#this should return the total number of record.
+lengthoftweets = viewDefnition('doc name here','totalnumber','''/
+    function(doc){
+        emit(doc.sa3_id, 1);
+    }''','''function(keys, values, rereduce){
+            return sum(values);
+    }
+    '''
+)
