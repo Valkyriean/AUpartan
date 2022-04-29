6ee0c54bfec97bdea01ea3df43d11bef172d6584
yantingMu <79830531+yantingMu@users.noreply.github.com>
2022-04-30 00:11:28 +1000

    added try except to handle no connection error

--- Worker/app.py
+++ Worker/app.py
@@ -9,21 +9,25 @@ import json
 
 
 REQUEST_GAP = 10
-GATEWAY_IP = "localhost"
-GATEWAY_PORT = 3000
+GATEWAY_IP = "172.26.128.147"
+GATEWAY_PORT = 5000
 ID=1
 
 while True:
     # Request task from main node
-    print("request task from gateway")
-    r = requests.post(("http://"+GATEWAY_IP+":"+str(GATEWAY_PORT)+"/task"), json={"id":1})
-    # Do job
-    res = r.json()
-    print(res)
-    if res["status"] == "success":
-        
-        print("get task id"+ str(res['task']['id']))
-        time.sleep(res["task"]["time"])
-    else:
-        print(res["status"])
-        time.sleep(REQUEST_GAP)
+    print("request task from gateway...")
+    try: 
+        r = requests.post(("http://"+GATEWAY_IP+":"+str(GATEWAY_PORT)+"/task"), json={"id": ID})
+        # Do job
+        res = r.json()
+        print(res)
+        if res["status"] == "success":
+            
+            print("get task id"+ str(res['task']['id']))
+            time.sleep(res["task"]["time"])
+        else:
+            print(res["status"])
+            time.sleep(REQUEST_GAP)
+    except:
+        print("no connection to gateway, retry in 1 sec")
+        time.sleep(1)
