668a2913a4d8ba9752826ef8b89f319ce8203593
Lupercal0 <55178796+Lupercal0@users.noreply.github.com>
2022-04-30 11:40:32 +1000

    making streaming1.py as a original backup
    original content of streaming.py is inside, the experiment 1 for streaming1.py is also inside, but as comment

--- backend/streaming1.py
+++ backend/streaming1.py
@@ -1,3 +1,54 @@
+from tweepy import StreamingClient, Tweet, StreamRule
+
+import secret
+
+bearer_token = secret.BEARER_TOKEN
+
+
+class TweetListener(StreamingClient):
+
+    def on_tweet(self, tweet: Tweet):
+        print(tweet.__repr__())
+        
+    def on_request_error(self, status_code):
+        print(status_code)
+    
+    def on_connection_error(self):
+        self.disconnect
+        
+client = TweetListener(bearer_token)
+
+rules = [
+    StreamRule(value="Melbourne lang:en")
+    # StreamRule(value=""),
+    # StreamRule(value="bounding_box:[144.3896 -38.5084 145.5459 -37.3127]")
+    
+]
+
+
+resp = client.get_rules()
+if resp and resp.data:
+    rule_ids = []
+    for rule in resp.data:
+        rule_ids.append(rule.id)
+    client.delete_rules(rule_ids)
+    
+    
+resp = client.add_rules(rules, dry_run= True)
+if resp.errors:
+    raise RuntimeError(resp.errors)
+
+resp = client.add_rules(rules)
+if resp.errors:
+    raise RuntimeError(resp.errors)
+
+print(client.get_rules())
+
+try:
+    client.filter()
+except KeyboardInterrupt:
+    client.disconnect()
+"""
 import tweepy
 import json
 import os
@@ -6,6 +57,7 @@ ACCESS_TOKEN = '931264431505358849-YzavFd1tCYRkkyrmbRuRkVAkmigYdWy'
 ACCESS_TOKEN_SECRET = 'n4NFnvIsRN3aSoeHsUGs82CNcxJIV6B4zASDjkFxno65O'
 CONSUMER_KEY = "K4leUSXBdJwHytayPXdFzEzJN"
 CONSUMER_SECRET = "bpyL6MhIsNKRoWDrHj0ou9wHwib7tCm4ob5p3SZam51iDZyqv4"
+
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True)
@@ -27,3 +79,4 @@ class MyListener(tweepy.Stream):
 
 stream = MyListener(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, max_retries=1)
 stream.filter(languages = ["en"], track=["Melbourne Covid"], locations = [143.072686, -37.766864, 145.137758, -37.216227])
+"""
