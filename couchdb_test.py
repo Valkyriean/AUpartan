ea60674484d66a61cf42cbdc8b98f523dd5a0c3c
Jiachen Li <alexjiachenli@hotmail.com>
2022-05-01 23:14:02 +1000

    Create couchdb_test.py

---
+++ couchdb_test.py
@@ -0,0 +1,19 @@
+import subprocess
+from couchdb import Server
+
+
+subprocess.check_call(["pip", "install", "CouchDB"])
+
+import couchdb
+
+
+DB_USERNAME="admin"
+DB_PASSWORD="meiyoumima"
+couch = Server()
+couch.resource.credentials = (DB_USERNAME, DB_PASSWORD)
+try:
+    db = couch['test']
+except:
+    db = couch.create('test')
+    
+db.save({'contant':'test'})
