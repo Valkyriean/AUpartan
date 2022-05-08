28970cfb6dc2f9eea3295c488dea002084fbb075
Lupercal0 <55178796+Lupercal0@users.noreply.github.com>
2022-05-08 20:35:48 +1000

    search method update, not finished

--- backend/tweet.py
+++ backend/tweet.py
@@ -1,33 +1,48 @@
-from flask import Blueprint
+#from flask import Blueprint
 import tweepy, os
-import app
+#import app
+from flaskext.couchdb import Document, CouchDBManager
+from couchdb.mapping import TextField, FloatField
 
-bp = Blueprint("tweet", __name__, url_prefix="/t")
+#bp = Blueprint("tweet", __name__, url_prefix="/t")
 BEARER_TOKEN = os.environ.get('BEARER_TOKEN', None)
 
 client = tweepy.Client(BEARER_TOKEN)
 
-@bp.route("/c")
+#@bp.route("/c")
 def check():
     return os.environ.get('BEARER_TOKEN', None)
 
 
-@bp.route("/s/<keyword>")
-def search(keyword):
+class SearchTweet(Document):
+    _id = TextField
+    #.....requirement attribute
+
+
+
+#@bp.route("/s/<keyword>")
+def search(keyword, db):
     result = client.search_recent_tweets(keyword, max_results=10)
     print(result.data)
-    ret = ""
+    #ret = ""
     for d in result.data:
-        if app.db_enable:
-            app.db.save({'id':d.id, 'text':d.text, 'maintag': keyword})
-        ret += "<h1>" + d.text + "</h1>"
+        #anti-dublication and write in process should be here
+
+
+
+
+
+        #db.save({'id':d.id, 'text':d.text, 'maintag': keyword})
+        #ret += "<h1>" + d.text + "</h1>"
     return ret
 
-@bp.route("/stream")
+
+"""
+#@bp.route("/stream")
 def stream():
     stream_client = tweepy.StreamingClient(BEARER_TOKEN)
     stream_client.add_rules("covid")
     res = stream_client.filter()
     print(res)
     return "check standard output"
-    
+""" 
