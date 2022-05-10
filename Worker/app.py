efa587d68bf4ef4d4ede44d26578835c4e9fa0f9
dqj1118 <78465501+dqj1118@users.noreply.github.com>
2022-05-11 00:18:28 +1000

    pass parameters for gateway ip and worker id

--- Worker/app.py
+++ Worker/app.py
@@ -8,13 +8,14 @@ from aurin import aurin_work
 from historic import historic_work
 from search import search_work
 import os 
+import sys
 
 # Initialise
 REQUEST_GAP = 10
-GATEWAY_IP = os.environ.get('GATEWAYIP', None)
+GATEWAY_IP = sys.argv[1]
 print(GATEWAY_IP)
 GATEWAY_PORT = 3000
-WORKER_ID = os.environ.get('WORKERIP', None)
+WORKER_ID = sys.argv[2]
 print(WORKER_ID)
 DB_USERNAME= "admin"
 DB_PASSWORD= "admin"
