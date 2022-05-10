a80d709c5ee62592ce95a358fc67b4a5d21fcfaf
dqj1118 <78465501+dqj1118@users.noreply.github.com>
2022-05-11 00:31:58 +1000

    change from localhost to 0.0.0.0

--- Gateway/app.py
+++ Gateway/app.py
@@ -214,4 +214,4 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="localhost", port=3000)
+    app.run(debug=True, host="0.0.0.0", port=3000)
