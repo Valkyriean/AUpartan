0376e150a580c353cbe494d86ee1b46a62a3d056
Lupercal0 <55178796+Lupercal0@users.noreply.github.com>
2022-05-13 03:32:17 +1000

    summary function done

--- Gateway/readSummary.py
+++ Gateway/readSummary.py
@@ -44,13 +44,14 @@ def extra_data_view(view_function, db):
 def extract_summary(couch, summary_db):
 
     if "aurin" in summary_db["name"]:
-        search_level = summary_db["name"].split("_")[1]
-        search_key = summary_db["name"].split("_")[2]
+        search_level = summary_db["name"].split("_")[0]
+        search_key = summary_db["name"].split("_")[1]
         for i in couch:
             if search_key in i:
                 if search_level in i:
-                    required_db = couch[i]
-                    break
+                    if "summary" in i:
+                        required_db = couch[i]
+                        break
         gateway_view = gateway_aurin(required_db, search_key)
     
     else:
@@ -60,8 +61,9 @@ def extract_summary(couch, summary_db):
         for i in couch:
             if search_method in i:
                 if search_key in i:
-                    required_db = couch[i]
-                    break
+                    if "summary" in i:
+                        required_db = couch[i]
+                        break
         gateway_view = gateway_tweet(required_db, search_key, search_method, search_stat)
     
     result_dict = extra_data_view(gateway_view, required_db)
