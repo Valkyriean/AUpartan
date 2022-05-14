fa87f90f3f2e571c313b7670300e0a2c1eeb4e9a
Jiachen Li <alexjiachenli@hotmail.com>
2022-05-14 10:25:56 +1000

    redone the backend
    now task list are recorded on db and no more communication between gateway and worker

--- Worker/app.py
+++ Worker/app.py
@@ -8,18 +8,16 @@ from historic import historic_work
 from search import search_work
 import sys
 from random import uniform
+from datetime import timedelta, datetime
+
 
 # Initialise
-REQUEST_GAP = 10
-GATEWAY_IP = "localhost"
-GATEWAY_PORT = 3000
+REQUEST_GAP = 15
 WORKER_IP = "localhost"
 DB_USERNAME= "admin"
 DB_PASSWORD= "admin"
-if len(sys.argv) == 3:
-    GATEWAY_IP = sys.argv[1]
-    WORKER_IP = sys.argv[2]
-print(GATEWAY_IP)
+if len(sys.argv) == 2:
+    WORKER_IP = sys.argv[1]
 print(WORKER_IP)
 
 couch = Server()
@@ -54,41 +52,166 @@ def assign_work(task):
     return False
 
 
+try:
+    pending = couch['pending']
+except:
+    pending = couch.create('pending')
 
+try:
+    working = couch['working']
+except:
+    working = couch.create('working')
+    
+try:
+    finished = couch['finished']
+except:
+    finished = couch.create('finished')
+timeout = timedelta(days=1)
+
+def try_work(task):
+    task['timeout'] = str(datetime.now()+timeout)
+    task.pop('_rev')
+    working.save(task)
+    print("Work on " + task['name'])
+    result = assign_work(task)
+    # result = True
+    if result:
+        working.delete(task)
+        task.pop('_rev')
+        finished.save(task)
+    else:
+        task.pop('_rev')
+        pending.save(task)
+
+# use three db
 def main():
     while True:
-        # Request task from main node
-        print("request task from gateway...")
-        try: 
-            r = requests.post(("http://"+GATEWAY_IP+":"+str(GATEWAY_PORT)+"/get_task"), json={"worker_ip": WORKER_IP})
-            res = r.json()
-            print(res)
-            if res.get("status", None) == "success":
-                print("get task name"+ str(res['task']['name']))
-                task = res.get("task",None)
-                task_name = task.get("name", "unnamed")
-                print(task)
-                if task:
-                    print("task valid")
-                    flag = assign_work(task)
-                    # flag = True
-                    # time.sleep(5)
-                    if flag:
-                        print("Finished")
-                        requests.post(("http://"+GATEWAY_IP+":"+str(GATEWAY_PORT)+"/finish_task"), json={"task_name": task_name})
-                    else:
-                        print("Failed")
-                        requests.post(("http://"+GATEWAY_IP+":"+str(GATEWAY_PORT)+"/failed_task"), json={"task_name": task_name})
-                else:
-                    print("NO task found")
-                    # task not exist
-                    # r = requests.post(("http://"+GATEWAY_IP+":"+str(GATEWAY_PORT)+"/failed_task"), json={"task_name": task_name})
-            else:
-                print(res["status"])
+        try:
+            # Try redo timeouted task
+            print("run")
+            if len(working) is not 0: 
+                print("have running")    
+                for t in working:
+                    task = working[t]
+                    if task['timeout'] < str(datetime.now()):
+                        print("found timeout task "+ t)
+                        try_work(task)
+                        break
+            # take task from pending
+            if len(pending) is 0:
+                print("no task left, go sleep")
                 time.sleep(uniform(REQUEST_GAP-0.5, REQUEST_GAP+0.5))
+                continue
+            else:
+                for t in pending:
+                    task = pending[t] 
+                    pre = task.get('prerequisite', None)
+                    if pre == None or pre in finished:
+                        print("found pending can run "+t)
+                        pending.delete(task)
+                        try_work(task)                    
+                    else:
+                        print("pre not ready "+t)
+                        time.sleep(uniform(REQUEST_GAP-0.5, REQUEST_GAP+0.5))
+            # time.sleep(uniform(REQUEST_GAP-0.5, REQUEST_GAP+0.5))
         except Exception as e: 
             print(repr(e))
-            print("no connection to gateway")
             time.sleep(uniform(REQUEST_GAP-0.5, REQUEST_GAP+0.5))
+            
+            
+            
+            
+tasks = [{"name": "aurin_preserve", 
+                "type" : "preserve",
+                "task" : "aurin"},
+{"name": "historic_preserve", 
+                "type" : "preserve",
+                "task" : "historic"},
+{"name": "aurin_payroll",
+                "type": "aurin",
+                "keyword": "payroll",
+                "level" : "sa3",
+                "prerequisite":"aurin_preserve"},
+{"name": "aurin_income",
+                "type": "aurin",
+                "keyword": "income",
+                "level" : "sa3",
+                "prerequisite":"aurin_preserve"},
+{"name": "aurin_immigration",
+                "type": "aurin",
+                "keyword": "immigration",
+                "level" : "city",
+                "prerequisite":"aurin_preserve"},
+{"name": "aurin_salary",
+                "type": "aurin",
+                "keyword": "salary",
+                "level" : "city",
+                "prerequisite":"aurin_preserve"},
+{"name": "search_election",
+                "type": "search",
+                "keyword": "Election"},
+{"name": "search_crime",
+                "type": "search",
+                "keyword": "crime"},
+{"name": "historic_crime",
+                "type": "historic",
+                "keyword": "crime",
+                "prerequisite":"historic_preserve"},
+{"name": "historic_all",
+                "type": "historic",
+                "keyword": "all",
+                "prerequisite":"historic_preserve"}]
+# for t in tasks:
+#     # if t['name'] not in pending:
+#         t['_id'] = t['name']
+#         t['state'] = 'pending'
+#         pending.save(t)
 
+# def work(task):
+#     task['timeout'] = str(datetime.now()+timeout)
+#     task['state'] = "working"
+#     print(task)
+#     # time.sleep(1)
+#     pending.save(task)
+#     print("try work on "+ task['name'])
+#     # result = assign_work(task)
+#     result = True
+#     time.sleep(5)
+#     if result:
+#         task['state'] = "finished"
+#         pending.save(task)
+#     else:
+#         task['state'] = "pending"
+#         pending.save(task)
+
+# use one db only 
+
+# def main():
+#     while True:
+#         if len(pending) == 0:
+#             print("no task left, go sleep")
+#             time.sleep(uniform(REQUEST_GAP-0.5, REQUEST_GAP+0.5))
+#             continue
+#         for t in pending:
+#             task = pending[t]
+#             if task['state'] == 'working' and task['timeout'] < str(datetime.now()):
+#                 print("found expired "+t)
+#                 work(task)
+#             if task['state'] == 'pending':
+#                 print("found pending "+t)
+#                 pre = pending[t].get('prerequisite', None)
+#                 if pre == None or pre not in pending or (pre in pending and pending[pre]['state'] == 'finished'):
+#                     print("found pending pre mate"+t)
+#                     work(task)                    
+#                 else:
+#                     print("pre not ready "+t)
+                
+#                 # continue
+#             time.sleep(uniform(REQUEST_GAP-0.5, REQUEST_GAP+0.5))
+
+# try:
+#     main()
+# except Exception as e: 
+#     print(repr(e))
+    
 main()
--- test/q_test.py
+++ test/q_test.py
@@ -1,9 +1,30 @@
 import queue
 
-a = queue.Queue()
-a.put(0)
-a.put(1)
+# a = queue.Queue()
+# a.put(0)
+# a.put(1)
 
-print(list(a.queue))
+# print(list(a.queue))
 
 
+queueing_task = queue.Queue()
+queueing_task.put({"name":"123"})
+# (due date, task, worker IP)
+working_task = [(0,{"name":"234"},0)]
+finished_task = ["456"]
+
+def check_global(task_name):
+    if check_finished(task_name):
+        return True
+    for t in list(queueing_task.queue):
+        if t.get("name",None) == task_name:
+            return True
+    for t in working_task:
+        if t[1].get("name",None) == task_name:
+            return True
+    return False
+
+def check_finished(task_name):
+    return task_name in finished_task
+
+print(check_global("124"))
