a4f83432a1d702dbb435c06e5c8ce030ae633df8
Jiachen Li <alexjiachenli@hotmail.com>
2022-05-16 21:10:20 +1000

    added author information

--- frontend/api/pytrsting.py
+++
@@ -1,6 +0,0 @@
-import queue
-queueing_task = queue.Queue()
-queueing_task.put(1)
-queueing_task.put(2)
-queueing_task.put(3)
-print(list(queueing_task))
--- frontend/src/components/SubmitWork/SubmitWork.js
+++ frontend/src/components/SubmitWork/SubmitWork.js
@@ -1,3 +1,10 @@
+// Group 1 Melbourne
+// Qianjun Ding 1080391
+// Zhiyuan Gao 1068184
+// Jiachen Li 1068299
+// Yanting Mu 1068314
+// Chi Zhang 1067750
+
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import './SubmitWork.css';
